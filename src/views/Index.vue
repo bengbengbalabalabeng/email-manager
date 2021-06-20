f11d502e6cd123ac9e69c9b20a6bacbb96b7e5bf
baicaixiaozhan <3083344240@qq.com>
2021-06-20 22:53:49 +0800

    commit

---
+++ src/views/Index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div id="nav">
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
